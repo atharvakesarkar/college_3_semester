bf3f6f1ebf47dd90ae261d6e87397ff56d055325
Atharva <athukesarkar@gmail>
2025-07-28 10:29:38 +0530

    use append method and then read it

--- file_handling.py
+++
@@ -1,6 +0,0 @@
-# r: open and exisiting file for a read operation
-# w: open an exisitng file for a write operation
-# a: open an existing file for appending data
-# r+ : open and existing file for both reading and writing
-# w+ : opne an exisiting file for both writing and reading
-# a+  open an existing file for both appending and reading
---
+++ python_programming/file_handling/file_handling.py
@@ -0,0 +1,22 @@
+# r: open and exisiting file for a read operation
+# w: open an exisitng file for a write operation
+# a: open an existing file for appending data
+# r+ : open and existing file for both reading and writing
+# w+ : opne an exisiting file for both writing and reading
+# a+  open an existing file for both appending and reading
+
+
+# file = open("geeks.txt","r")
+# for each in file:
+#     print(each)
+
+
+## this is the append methond and this is working on my python ide
+# file = open("atharva.txt", "a")  # Open file in append mode
+# file.write("siidhi,niyati,om,adityabhau")  # This works fine
+# file.close()  # Always close the file after writing
+
+# # Now open the file again in read mode to read its contents
+# file = open("atharva.txt", "r")
+# print(file.read())
+
