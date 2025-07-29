079267025d1d7f4dd465d7312526244dd9fe3109
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-07-29 10:24:30 +0530

    Update for.py

--- python_programming/for.py
+++ python_programming/for.py
@@ -1,31 +1,38 @@
-# fruits = ["apple", "pineapple","ornage"]
-# for i in fruits:
-#     print(i)
+fruits = ["apple", "pineapple","ornage"]
+for i in fruits:
+    print(i)
 
 
 # for loop using range function
-# n = 4
-# for i in range(0,n):
-#     print(i)
+n = 4
+for i in range(0,n):
+    print(i)
 
 
 # for loop for different data types:
 
-# li = ["geeks","for","geeks"]
-# for i in li:
-#     print(i)
+li = ["geeks","for","geeks"]
+for i in li:
+    print(i)
 
 
-# tup = ("geeks","for","geeks")
-# for i in tup:
-#     print(i)
+tup = ("geeks","for","geeks")
+for i in tup:
+    print(i)
 
-# d = dict({"x":123,"y":234})
-# for i in d:
-#     print("%s %d" % (i, d[i]))
+d = dict({"x":123,"y":234})
+for i in d:
+    print("%s %d" % (i, d[i]))
 
-# set1 = {1,2,3,4,5,6,7,8}
-# for i in set1:
-#     print(i)
+set1 = {1,2,3,4,5,6,7,8}
+for i in set1:
+    print(i)
+
+
+# iterting by index of sequences
+li = ["geeks","for","geeks"]
+for index in range (len(li)):
+    print(li[index]) 
+
 
 
