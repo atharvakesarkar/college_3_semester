46fb23a5e00c86a70167fa43ea248abfe7f82f5c
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-07-29 10:18:46 +0530

    Update for.py

--- python_programming/for.py
+++ python_programming/for.py
@@ -4,6 +4,28 @@
 
 
 # for loop using range function
-n = 4
-for i in range(0,n):
-    print(i)
+# n = 4
+# for i in range(0,n):
+#     print(i)
+
+
+# for loop for different data types:
+
+# li = ["geeks","for","geeks"]
+# for i in li:
+#     print(i)
+
+
+# tup = ("geeks","for","geeks")
+# for i in tup:
+#     print(i)
+
+# d = dict({"x":123,"y":234})
+# for i in d:
+#     print("%s %d" % (i, d[i]))
+
+# set1 = {1,2,3,4,5,6,7,8}
+# for i in set1:
+#     print(i)
+
+
