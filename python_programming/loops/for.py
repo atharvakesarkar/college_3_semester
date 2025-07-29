b2a6748b68c3d468353ced79c906517535ec9b49
Atharva <athukesarkar@gmail>
2025-07-29 21:13:07 +0530

    Save while and for loop changes

---
+++ python_programming/loops/for.py
@@ -0,0 +1,67 @@
+# fruits = ["apple", "pineapple","ornage"]
+# for i in fruits:
+#     print(i)
+
+
+# for loop using range function
+# n = 4
+# for i in range(0,n):
+#     print(i)
+
+
+# for loop for different data types:
+
+# li = ["geeks","for","geeks"]
+# for i in li:
+#     print(i)
+
+
+# tup = ("geeks","for","geeks")
+# for i in tup:
+#     print(i)
+
+# d = dict({"x":123,"y":234})
+# for i in d:
+#     print("%s %d" % (i, d[i]))
+
+# set1 = {1,2,3,4,5,6,7,8}
+# for i in set1:
+#     print(i)
+
+
+
+
+# iterting by index of sequences
+# li = ["geeks","for","geeks"]
+# for index in range (len(li)):
+#     print(li[index]) 
+
+
+# using else statement with loop in python
+# li = ["geeks","for","geeks"]
+# for index in range (len(li)):
+#     print(li[index]) 
+# else:
+#     print("Inside the block")
+
+
+
+# # using of continue statement
+# for letter in "geeksforgeeks":
+#     if letter == 'e' or letter == 's':
+#         continue
+#     print('current letter: ',letter)
+
+
+# # break statement
+# for letter in 'geeksforgeeks':
+#     if letter == 'e' or letter == 's':
+#         break
+# print('current letter: ', letter)
+
+
+
+# # pass statement:
+# for letter in 'geeksforgeeks':
+#     pass
+# print('last letter: ',letter)
---
+++ python_programming/loops/while.py
@@ -0,0 +1,23 @@
+# count = 0
+# while count < 5:
+#     print(count)
+#     count +=1
+
+
+
+# using else statement with while loop
+# count = 0
+# while (count < 3):
+#     count = count + 1
+#     print("hello geek")
+# else:
+#     print("In Else block")
+
+
+
+# # infinte loop
+# while(True):
+#     print("i want to be the good programmer")
+
+
+
