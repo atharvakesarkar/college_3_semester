e35d46d88b01a9a5c639bc86e31491824e05d6d1
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-07-29 10:37:02 +0530

    Update while.py

--- python_programming/while.py
+++ python_programming/while.py
@@ -2,3 +2,12 @@ count = 0
 while count < 5:
     print(count)
     count +=1
+
+
+# using else statement with while loop
+count = 0
+while (count < 3):
+    count = count + 1
+    print("hello geek")
+else:
+    print("In Else block")
